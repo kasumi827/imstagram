fb3a655c9488d8a5cb32ddebb51816d5939959f8
kasumi827 <kasumiuchan827@gmail.com>
2021-10-27 03:05:38 +0900

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
     def show
         @user = User.find_by(id: params[:id])
+        # @user = current_user.id
       end
 end
